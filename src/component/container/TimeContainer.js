be7db9da46366c82b3e3067cdd73ff31a1e289ff
Shahabuddin <ansari.extc@gmail.com>
2021-12-08 19:00:27 +0530

    udpated

--- src/component/container/TimeContainer.js
+++ src/component/container/TimeContainer.js
@@ -6,6 +6,10 @@ export const TimeContainer = () => {
         <>
         <p>Time</p>
             <DigitalClock />
+            <div className="timer-stopWatch">
+                <p className="time-container-timer">Timer</p>
+                <p className="time-container-stopWatch">Stop watch</p>
+            </div>
         </>
     );
 }
